9adb91c7ac561220c339f0cddabce7566e37928a
Brandon Boudreaux <hf3209@wayne.edu>
2023-07-14 11:10:46 -0400

    Data loading

---
+++ vectorsml/dataset.py
@@ -0,0 +1,42 @@
+import numpy as np
+import torch
+from vectorsml.result import Result
+from torch.utils.data import Dataset as ds
+
+import h5py
+
+class Dataset(ds):
+    def __init__(self, file):
+        super(Dataset, self).__init__()
+
+        self.results = []
+
+        data = h5py.File(file)
+
+        def getInitalCompTensor(result):
+            for i in result.keys():
+                if "eccentricities" in i: 
+                    initial = result[i]
+                    comp_tens = torch.transpose(torch.tensor(initial[:]), 0, 1)
+                    initial_tensor = comp_tens[4] + 1j*comp_tens[5]
+                    return initial_tensor
+                
+        def getFinalCompTensor(result):
+            final = result['particle_9999_dNdeta_pT_0.2_3.dat']
+            comp_tens = torch.transpose(torch.tensor(final[:]), 0, 1)
+            final_tensor = comp_tens[4] + 1j*comp_tens[5]
+            return final_tensor
+
+
+        for v in data.keys():
+            spvn_result = data[v]
+
+            self.results.append(
+                Result(v, getInitalCompTensor(spvn_result), getFinalCompTensor(spvn_result))
+            )
+
+    def __len__(self):
+        return len(self.results)
+
+    def __getitem__(self, item):
+        return self.results[item]
---
+++ vectorsml/result.py
@@ -0,0 +1,5 @@
+class Result:
+    def __init__(self, id, key, value) -> None:
+        self.id     = id
+        self.key    = key
+        self.value  = value
